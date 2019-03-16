d8335025f9dadbc34d8afe666489a7d7c0e00190
Đô Đô <do.nguyen.intern@ntq-solution.com.vn>
2019-03-16 16:38:04 +0700

    Revert "change variable color"
    This reverts commit 31339787bb753bde84136affea166ef50d4fa116.

--- src/App.vue
+++ src/App.vue
@@ -21,3 +21,12 @@ import footerComponent from "./components/footer/footer.component.vue";
 })
 export default class App extends Vue {}
 </script>
+
+<style lang='scss'>
+#app {
+  font-family: "Montserrat", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
